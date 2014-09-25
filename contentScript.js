5d60c24b3a6b4669e5ce8756e12657bac85648dc
Ryan Miller <rmill865@gmail.com>
2014-09-25 08:35:55 +0200

    Finished???

--- contentScript.js
+++ contentScript.js
@@ -1,12 +1,15 @@
 /**
  * This script is run when Grooveshark first loads. It's purpose is
  * to inject script.js to the page. This is necessary because we
- * need access to the window.Grooveshark attribute (Grooveshark JS API) 
+ * need to be able to trigger events on the page's DOM 
  */
 
+// Append Jquery to the page
+var jquery = document.createElement('script');
+jquery.src = chrome.extension.getURL('library/jquery-2.1.1.min.js');
+(document.head||document.documentElement).appendChild(jquery);
+
+// Append out script to the page
 var script = document.createElement('script');
 script.src = chrome.extension.getURL('script.js');
 (document.head||document.documentElement).appendChild(script);
-script.onload = function() {
-    script.parentNode.removeChild(script);
-};
--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 /**
- * This script will touch the API after every song status change. This
- * keeps the 'Are you still listening?' modal from popping up
+ * This script kills the resume modal before it can be shown.
  */
-var readyStateCheckInterval = setInterval(function() {
-    if (window.Grooveshark !== undefined) {
-        clearInterval(readyStateCheckInterval);
 
-        // Party On
-        var GroovesharkApi = window.Grooveshark;
-        GroovesharkApi.setSongStatusCallback(function() {
-            // Click the resume buttin if it exists
-            $('[data-translate-text="LB_INTERACTION_TIME_RESUME"]').click();
-        })
-    }
+// Make sure Jquery has loaded
+var jqueryInterval = setInterval(function () {
+	if (window.jQuery) {
+		clearInterval(jqueryInterval);
+
+		// When ever the modal DOM changes, check if it is the time resume modal and close it
+		$('#lightbox-outer').on('DOMSubtreeModified', function() {
+		    var resumeModal = $(this).find('.lightbox-interactionTimeout');
+		    
+		    if (resumeModal.length > 0) {
+		        $(resumeModal).find('[data-translate-text="LB_INTERACTION_TIME_RESUME"]').click();
+		    }
+		});
+	}
 }, 10);
